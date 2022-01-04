b7ec1228a8084b139de74b07d008a6d063c36118
henry warui <warui.henry@gmail.com>
2022-01-04 13:50:57 +0300

    add usequery for fetching data

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,26 @@
+// Fetching Data with useQuery
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+  return axios.get("http://localhost:4000/superheroes");
+};
+
 export const RQSuperHeroPage = () => {
-  return <div>RQ Super Heroes Page </div>;
+  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+
+  if (isLoading) {
+    return <h2>Loading ...</h2>;
+  }
+
+  console.log({ data });
+
+  return (
+    <>
+      <h2>RQ Super Heroes Page </h2>
+      {data?.data.map((hero) => {
+        return <div key={hero.name}>{hero.name}</div>;
+      })}
+    </>
+  );
 };
