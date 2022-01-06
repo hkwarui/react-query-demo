2ffbb1e97e3137803410f865a1e720b445634662
henry warui <warui.henry@gmail.com>
2022-01-06 10:42:20 +0300

    success and error callbacks

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -3,15 +3,23 @@ import { useQuery } from "react-query";
 import axios from "axios";
 
 const fetchSuperHeroes = () => {
-  return axios.get("http://localhost:4000/superheroes");
+  return axios.get("http://localhost:4000/superheroes1");
 };
 
 export const RQSuperHeroPage = () => {
+  const onSuccess = (data) => {
+    console.log("Perform side effect after data fetching", data);
+  };
+  const onError = (error) => {
+    console.log("Perform side effect after encoutering an error ", error);
+  };
   const { isLoading, data, isError, error, isFetching, refetch } = useQuery(
     "super-heroes",
     fetchSuperHeroes,
     {
       enabled: false,
+      onSuccess,
+      onError,
     }
   );
   console.log({ isLoading, isFetching });
